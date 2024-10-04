71f2953d7d89805fd0e2ac69ab4c3fc8e7cec8f5
Adel <snakeadel00@gmail.com>
2024-10-04 09:03:14 +0300

    complete lesson 12

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,19 @@
-import React, {Suspense} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
-import './styles/index.scss';
-import {classNames} from "shared/lib/classNames/classNames";
-import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
-import { AppRouter } from './providers/router';
-
+import "./styles/index.scss";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "app/providers/ThemeProvider";
+import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
-   const { theme, toggleTheme } = useTheme();
+  const { theme, toggleTheme } = useTheme();
 
-    return (
-        <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
-            <AppRouter />
-        </div>
-    );
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <Navbar />
+      <AppRouter />
+      <button onClick={toggleTheme}>TOGGLE</button>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+import { Link } from "react-router-dom";
+import cls from "./Navbar.module.scss";
+import { classNames } from "shared/lib/classNames/classNames";
+import { AppLink, AppLinkTheme } from "shared/ui";
+
+interface INavbarProps {
+  className?: string;
+}
+
+export const Navbar: FC<INavbarProps> = ({ className }) => {
+  return (
+    <header className={classNames(cls.navbar, {}, [className])}>
+      <nav className={cls.links}>
+        <AppLink theme={AppLinkTheme.SECONDARY} className={cls.mainLink} to={"/"}>
+          Главная
+        </AppLink>
+        <AppLink theme={AppLinkTheme.RED} to={"/about"}>О сайте</AppLink>
+      </nav>
+    </header>
+  );
+};
