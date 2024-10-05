9ad9f7bbaef8d80c4f77d4c1726d766a68b42056
Adel Sharipov <snakeadel00@gmail.com>
2024-10-05 19:06:24 +0300

    add react-refresh webpack plugn (lesson 16)

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
 
 
 
@@ -18,6 +19,7 @@ export const buildPlugins = ({paths, isDev}: BuildOptions): webpack.WebpackPlugi
         new webpack.DefinePlugin({
           __IS_DEV__: JSON.stringify(isDev),
         }),
+        new ReactRefreshWebpackPlugin(),
         new webpack.HotModuleReplacementPlugin(),
       ]
 }
