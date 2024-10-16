1257f98bf412c259c2d875e60341c8eab4b937d7
Adel <snakeadel00@gmail.com>
2024-10-16 13:56:30 +0300

    complete lesson 38

--- src/app/providers/StoreProvider/index.ts
+++ src/app/providers/StoreProvider/index.ts
@@ -1,3 +1,3 @@
 export { StoreProvider } from './ui/StoreProvider';
-export { StateSchema, ReduxStoreWithManager } from './config/StateSchema';
+export { StateSchema, ReduxStoreWithManager, ThunkConfig } from './config/StateSchema';
 export { createReduxStore, AppDispatch } from './config/store';
--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -3,25 +3,24 @@ import { Provider } from 'react-redux';
 import { createReduxStore } from 'app/providers/StoreProvider';
 import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
 import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
+import { useNavigate } from 'react-router-dom';
 
 interface StoreProviderProps {
-    children?: ReactNode;
-    initialState?: DeepPartial<StateSchema>;
-    asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
+  children?: ReactNode;
+  initialState?: DeepPartial<StateSchema>;
+  asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>;
 }
 
 export const StoreProvider = (props: StoreProviderProps) => {
-    const {
-        children,
-        initialState,
-        asyncReducers,
-    } = props;
+    const { children, initialState, asyncReducers } = props;
 
-    const store = createReduxStore(initialState as StateSchema, asyncReducers as ReducersMapObject<StateSchema>);
+    const navigate = useNavigate();
 
-    return (
-        <Provider store={store}>
-            {children}
-        </Provider>
+    const store = createReduxStore(
+    initialState as StateSchema,
+    asyncReducers as ReducersMapObject<StateSchema>,
+    navigate,
     );
+
+    return <Provider store={store}>{children}</Provider>;
 };
