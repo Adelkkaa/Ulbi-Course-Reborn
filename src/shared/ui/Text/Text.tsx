c75dd6f18a5ae9e7ff64e9aadc6e6f4e8123f0ae
Adel <snakeadel00@gmail.com>
2024-10-25 22:20:19 +0300

    complete lesson 72

--- src/shared/ui/Text/Text.tsx
+++ src/shared/ui/Text/Text.tsx
@@ -26,6 +26,8 @@ interface TextProps {
     theme?: TextTheme;
     align?: TextAlign;
     size?: TextSize;
+
+    'data-testid'?: string;
 }
 
 type HeaderTagType = 'h1' | 'h2' | 'h3';
@@ -44,6 +46,7 @@ export const Text = (props: TextProps) => {
         align = TextAlign.LEFT,
         theme = TextTheme.PRIMARY,
         size = TextSize.M,
+        'data-testid': dataTestId = 'Text',
     } = props;
 
     const HeaderTag = mapSizeToHeaderTag[size];
@@ -56,8 +59,22 @@ export const Text = (props: TextProps) => {
 
     return (
         <div className={classNames(cls.Text, mods, [className])}>
-            {title && <HeaderTag className={cls.title}>{title}</HeaderTag>}
-            {text && <p className={cls.text}>{text}</p>}
+            {title && (
+                <HeaderTag
+                    data-testid={`${dataTestId}.Header`}
+                    className={cls.title}
+                >
+                    {title}
+                </HeaderTag>
+            )}
+            {text && (
+                <p
+                    data-testid={`${dataTestId}.Paragraph`}
+                    className={cls.text}
+                >
+                    {text}
+                </p>
+            )}
         </div>
     );
 };
