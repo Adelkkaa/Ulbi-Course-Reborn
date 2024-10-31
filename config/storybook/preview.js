ba050915cfba85e9af17f306c39d6ad771c45684
Adel <snakeadel00@gmail.com>
2024-10-31 13:46:35 +0300

    complete lesson 97

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,20 +1,19 @@
-import { addDecorator } from '@storybook/react';
-import { Theme } from '@/shared/const/theme';import { RouterDecorator } from '@/shared/config/storybook/RouterDecorator/RouterDecorator';
-import { StyleDecorator } from '@/shared/config/storybook/StyleDecorator/StyleDecorator';
-import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
-import { TranslationDecorator } from '@/shared/config/storybook/TranslationDecorator/TranslationDecorator';
-import {
-    SuspenseDecorator,
-} from '@/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
+import { addDecorator } from "@storybook/react";
+import { Theme } from "@/shared/const/theme";
+import { RouterDecorator } from "@/shared/config/storybook/RouterDecorator/RouterDecorator";
+import { StyleDecorator } from "@/shared/config/storybook/StyleDecorator/StyleDecorator";
+import { ThemeDecorator } from "@/shared/config/storybook/ThemeDecorator/ThemeDecorator";
+import { TranslationDecorator } from "@/shared/config/storybook/TranslationDecorator/TranslationDecorator";
+import { SuspenseDecorator } from "@/shared/config/storybook/SuspenseDecorator/SuspenseDecorator";
 
 export const parameters = {
-    actions: { argTypesRegex: '^on[A-Z].*' },
-    controls: {
-        matchers: {
-            color: /(background|color)$/i,
-            date: /Date$/,
-        },
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
     },
+  },
 };
 
 addDecorator(StyleDecorator);
@@ -22,4 +21,3 @@ addDecorator(ThemeDecorator(Theme.LIGHT));
 addDecorator(RouterDecorator);
 addDecorator(TranslationDecorator);
 addDecorator(SuspenseDecorator);
-
