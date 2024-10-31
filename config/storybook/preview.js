948249918578df49f3ffcfffeb390868053c52a2
Adel <snakeadel00@gmail.com>
2024-10-31 14:22:10 +0300

    complete lesson 99

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -14,8 +14,15 @@ export const parameters = {
       date: /Date$/,
     },
   },
-  layout: 'fullscreen',
-
+  themes: {
+    default: "light",
+    list: [
+      { name: "light", class: Theme.LIGHT, color: "#ffffff" },
+      { name: "dark", class: Theme.DARK, color: "#000000" },
+      { name: "orange", class: Theme.ORANGE, color: "#ffb005" },
+    ],
+  },
+  layout: "fullscreen",
 };
 
 addDecorator(StyleDecorator);
