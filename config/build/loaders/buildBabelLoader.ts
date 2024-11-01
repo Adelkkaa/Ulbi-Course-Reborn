78baa9f736b00cba4968cdc3117c05de5e9ddd2f
Adel <snakeadel00@gmail.com>
2024-11-01 09:41:26 +0300

    fix pipeline error

--- config/build/loaders/buildBabelLoader.ts
+++ config/build/loaders/buildBabelLoader.ts
@@ -14,6 +14,7 @@ export function buildBabelLoader({ isDev, isTsx }: BuildBabelLoaderProps) {
         use: {
             loader: 'babel-loader',
             options: {
+                cacheDirectory: true,
                 presets: ['@babel/preset-env'],
                 plugins: [
                     [
